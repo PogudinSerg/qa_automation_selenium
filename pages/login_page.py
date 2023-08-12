3d959343b03285f954b8896978636942bd8f9381
SerG QA <43043184+PogudinSerg@users.noreply.github.com>
2023-08-12 21:17:28 +0300

    change test cases to correct transfer between pages

--- pages/login_page.py
+++ pages/login_page.py
@@ -12,8 +12,8 @@ class LoginPage(BasePage):
         assert "login" in self.browser.current_url, "Wrong login url"
 
     def should_be_login_form(self):
-        assert self.browser.is_element_present(*LoginPageLocators.LOGIN_FORM) is True, "Login form didn't appeared"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM) is True, "Login form didn't appeared"
 
     def should_be_register_form(self):
-        assert self.browser.is_element_present(*LoginPageLocators.REGISTER_FORM) is True, \
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM) is True, \
             "Register form didn't appeared"
